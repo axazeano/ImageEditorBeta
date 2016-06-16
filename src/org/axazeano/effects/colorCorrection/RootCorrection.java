ef8c5b33540e824d88aabad45c0a4459536dceb6
Vladimir Kubantsev <vovchik161@gmail.com>
2016-06-16 10:10:52 +0300

    Fixed errors in RootCorrection

--- src/org/axazeano/effects/colorCorrection/RootCorrection.java
+++ src/org/axazeano/effects/colorCorrection/RootCorrection.java
@@ -1,6 +1,7 @@
 package org.axazeano.effects.colorCorrection;
 
-import javafx.scene.paint.Color;
+import org.axazeano.ARGBColor;
+import org.axazeano.EditableImage;
 
 /**
  * Created by vladimir on 01.06.2016.
@@ -11,31 +12,28 @@ public class RootCorrection extends FunctionalCorrection {
         name = "Root correction";
     }
 
-    @Override
-    protected void preEvaluating() {
-        coefRed = maxRed / 15.95;
-        coefGreen = maxGreen / 15.95;
-        coefBlue = maxBlue / 15.95;
+    public RootCorrection(EditableImage image) {
+        super(image);
     }
 
     @Override
-    public void proceedEffect(){
-        super.proceedEffect();
-        int resultRed;
-        int resultGreen;
-        int resultBlue;
+    protected void prepareEvaluatingCoefficients() {
+        redCoefficient = maxRed / 15.95;
+        greenCoefficient = maxGreen / 15.95;
+        blueCoefficient = maxBlue / 15.95;
+    }
 
+    @Override
+    public int[] performEffect() {
         for (int y=0; y < selection.getHeight(); y++) {
             for (int x=0; x < selection.getWidth(); x++) {
-                Color currentPixel = pixelReader.getColor(x, y);
-                resultRed = (int) Math.sqrt((currentPixel.getRed() * 255) * coefRed);
-                resultGreen = (int) Math.sqrt((currentPixel.getGreen() * 255) * coefGreen);
-                resultBlue = (int) Math.sqrt((currentPixel.getBlue() * 255) * coefBlue);
-
-                pixelWriter.setColor(x, y, Color.rgb(resultRed < 256 ? resultRed : 255,
-                        resultGreen < 256 ? resultGreen : 255,
-                        resultBlue < 255 ? resultBlue : 255));
+                ARGBColor color = new ARGBColor(sourcePixelArray[x + y * inputImage.getWeight()]);
+                color.setRed((int) Math.sqrt((color.getRed() * 255) * redCoefficient));
+                color.setGreen((int) Math.sqrt((color.getGreen() * 255) * greenCoefficient));
+                color.setBlue((int) Math.sqrt((color.getBlue() * 255) * blueCoefficient));
+                targetPixelArray[x + y * inputImage.getWeight()] = color.getColor();
             }
         }
+        return targetPixelArray;
     }
 }
