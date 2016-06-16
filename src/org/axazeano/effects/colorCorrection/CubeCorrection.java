0300b80de0255de44d950558cb13de80cb0d36e3
Vladimir Kubantsev <vovchik161@gmail.com>
2016-06-16 09:49:00 +0300

    Fix errors in CubeCorrection

--- src/org/axazeano/effects/colorCorrection/CubeCorrection.java
+++ src/org/axazeano/effects/colorCorrection/CubeCorrection.java
@@ -1,41 +1,39 @@
 package org.axazeano.effects.colorCorrection;
 
-import javafx.scene.paint.Color;
+import org.axazeano.ARGBColor;
+import org.axazeano.EditableImage;
 
 /**
  * Created by vladimir on 01.06.2016.
  */
-public class CubeCorrection  extends FunctionalCorrection {
+public class CubeCorrection extends FunctionalCorrection {
     static {
         description = "Cube correction";
         name = "Cube correction";
     }
 
-    @Override
-    protected void preEvaluating() {
-        coefRed = maxRed / (255 * 255 * 255);
-        coefGreen = maxGreen / (255 * 255 * 255);
-        coefBlue = maxBlue / (255 * 255 * 255);
+    public CubeCorrection(EditableImage image) {
+        super(image);
     }
 
     @Override
-    public void proceedEffect(){
-        super.proceedEffect();
-        int resultRed;
-        int resultGreen;
-        int resultBlue;
+    protected void prepareEvaluatingCoefficients() {
+        redCoefficient = maxRed / (255 * 255 * 255);
+        greenCoefficient = maxGreen / (255 * 255 * 255);
+        blueCoefficient = maxBlue / (255 * 255 * 255);
+    }
 
+    @Override
+    public int[] performEffect() {
         for (int y=0; y < selection.getHeight(); y++) {
             for (int x=0; x < selection.getWidth(); x++) {
-                Color currentPixel = pixelReader.getColor(x, y);
-                resultRed = (int) (Math.pow((currentPixel.getRed() * 255), 3) * coefRed);
-                resultGreen = (int) (Math.pow((currentPixel.getGreen() * 255), 3) * coefGreen);
-                resultBlue = (int) (Math.pow((currentPixel.getBlue() * 255), 3) * coefBlue);
-
-                pixelWriter.setColor(x, y, Color.rgb(resultRed < 256 ? resultRed : 255,
-                        resultGreen < 256 ? resultGreen : 255,
-                        resultBlue < 255 ? resultBlue : 255));
+                ARGBColor color = new ARGBColor(sourcePixelArray[x + y * inputImage.getWeight()]);
+                color.setRed((int) (Math.pow((color.getRed() * 255), 3) * redCoefficient));
+                color.setGreen((int) (Math.pow((color.getGreen() * 255), 3) * greenCoefficient));
+                color.setBlue((int) (Math.pow((color.getBlue() * 255), 3) * blueCoefficient));
+                targetPixelArray[x + y * inputImage.getWeight()] = color.getColor();
             }
         }
+        return targetPixelArray;
     }
 }
